e8c46c89eefc807eca7379d90ed93a147fdf7a04
Nikolaj Goodger <Nikolaj.Goodger@teaam.telstra.com>
2017-11-18 10:12:25 +1100

    Fixed style issues in feed_forward_policy.py

--- policies/feed_forward_policy.py
+++ policies/feed_forward_policy.py
@@ -27,30 +27,39 @@ class FeedForwardPolicy(PolicyBase):
         with tf.variable_scope('variables', reuse=False):
             for i, layer_size in enumerate(hidden_size):
                 hidden_mean_in = (self.state if i == 0
-                                     else self.hidden_mean_output[-1])
+                                  else self.hidden_mean_output[-1])
                 hidden_stddev_in = (self.state if i == 0
-                                     else self.hidden_stddev_output[-1])
+                                    else self.hidden_stddev_output[-1])
                 mean_layer_name = "mean_hidden_{}".format(i)
-                self.mean_hidden.append(tf.layers.dense(inputs=hidden_mean_in,
-                                                        units=layer_size,
-                                                        activation=tf.nn.relu,
-                                                        name=mean_layer_name))
-                self.stddev_hidden.append(tf.layers.dense(inputs=hidden_mean_input,
-                                                          units=layer_size,
-                                                          activation=tf.nn.relu,
-                                                          name="stddev_hidden_{}".format(i)))
+                stddev_layer_name = "stddev_hidden_{}".format(i)
+                mean_layer = tf.layers.dense(inputs=hidden_mean_in,
+                                             units=layer_size,
+                                             activation=tf.nn.relu,
+                                             name=mean_layer_name)
+                self.mean_hidden.append(mean_layer)
+                stddev_layer = tf.layers.dense(inputs=hidden_stddev_in,
+                                               units=layer_size,
+                                               activation=tf.nn.relu,
+                                               name=stddev_layer_name)
+                self.stddev_hidden.append(stddev_layer)
             """
-            OUTPUTS 
+            OUTPUTS
             """
-            mean_input = self.state if len(hidden_size) == 0 else self.hidden_mean_output[-1]
-            stddev_input = self.state if len(hidden_size) == 0 else self.hidden_stddev_output[-1]
-            self.mean= tf.layers.dense(inputs=mean_input, units=action_size, name="mean")
-            self.stddev = tf.layers.dense(inputs=stddev_input, units=action_size, name="stddev")
-        self.normal_dist = tf.contrib.distributions.Normal(self.mean, self.stddev)
+            mean_input = (self.state if len(hidden_size) == 0
+                          else self.hidden_mean_output[-1])
+            stddev_input = (self.state if len(hidden_size) == 0
+                            else self.hidden_stddev_output[-1])
+            self.mean = tf.layers.dense(inputs=mean_input,
+                                        units=action_size, name="mean")
+            self.stddev = tf.layers.dense(inputs=stddev_input,
+                                          units=action_size, name="stddev")
+        self.normal_dist = tf.contrib.distributions.Normal(self.mean,
+                                                           self.stddev)
         self.action = self.normal_dist._sample_n(1)
         self.action_clipped = tf.clip_by_value(self.action,
-                                       clip_value_min=-1.0,
-                                       clip_value_max=1.0, name="action")
+                                               clip_value_min=-1.0,
+                                               clip_value_max=1.0,
+                                               name="action")
         self.save_idx = 0
 
     def setup(self, saved_policy_path: str=""):
@@ -72,11 +81,11 @@ class FeedForwardPolicy(PolicyBase):
         variable_summaries(stddev_bias)
         self.merged = tf.summary.merge_all()
         self.train_writer = tf.summary.FileWriter("./train",
-                                                      self.sess.graph)
+                                                  self.sess.graph)
 
     def choose_action(self, state: np.array) -> np.array:
-        return self.sess.run((self.action, self.action_clipped), {self.state: state})
-
+        return self.sess.run((self.action, self.action_clipped),
+                             {self.state: state})
 
     def adjust(self, state: np.array, target: float, action: np.array):
         feed_dict = {self.state: state, self.target: target,
@@ -90,7 +99,7 @@ class FeedForwardPolicy(PolicyBase):
         return step_loss
 
     def save_tensorboard(self, episode_reward: float) -> None:
-        feed_dict={self.episode_reward: episode_reward}
+        feed_dict = {self.episode_reward: episode_reward}
         summary = self.sess.run(self.merged, feed_dict)
         self.train_writer.add_summary(summary, self.save_idx)
         self.save_idx += 1
